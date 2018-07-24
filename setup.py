fc5a1e2823ce246798f6a11d49b6c1e58a36d669
wxnacy <371032668@qq.com>
2018-07-24 17:55:23 +0800

    add

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'wwx',
-    version = '0.0.2',
+    version = '0.0.4',
     keywords='wx',
     description = 'a library for wx Developer',
     license = 'MIT License',
